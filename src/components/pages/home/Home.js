25dcdfd4c1188daa4c848e7063a420d190391648
ArthurSilveira <arthur.saboya@gmail.com>
2019-01-17 10:07:10 -0500

    adds hero and fixes a few things

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import './Home.css'
+import  heroImage from '../../../assets/images/hero.jpg';
 
 const Home = () => (
   <div className='home-page page-wrapper'>
+    <div class="hero-image">
+      <img src={heroImage} alt="404"/>
+    </div>
     <div className="page-section center-row">
       <div className="page-title">RECLAIMED</div>
       <div>Home page stuff</div>
